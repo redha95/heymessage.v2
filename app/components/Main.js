c6eddb8abf81cff6de533f2619cbcb23834bf3fc
Redha AISSAOUI <redha955@hotmail.fr>
2018-04-05 10:12:12 +0200

    1er TEST

---
+++ app/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity,  } from 'react-native';
+
+export default class App extends React.Component {
+render() {
+  return (
+    <View><Text> 1er TEST</Text></View>
+  );
+ }
+}
+
+const styles = StyleSheet.create({
+  container: { 
+
+  },
+});  
